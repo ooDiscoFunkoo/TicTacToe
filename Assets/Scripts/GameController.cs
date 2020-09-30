db85a2e2440d003fd6b2aa099c4dc52fb8082948
Mike Ross <Radeesh@Mikes-MBP.fritz.box>
2020-10-01 12:13:28 +1300

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Text[] buttonList;
+
+    private string playerSide;
+
+    void Awake ()
+    {
+        SetGameControllerReferenceOnButtons();
+        playerSide = "X";
+
+    }
+
+    void SetGameControllerReferenceOnButtons ()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+            // Debug.Log(i);
+        }
+    }
+
+    public string GetPlayerSide ()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn ()
+    {
+        if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver ()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+    }
+}
